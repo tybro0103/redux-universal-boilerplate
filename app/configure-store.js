d30d2bf4169f8dae90f87803624710513e1218cd
Tyler Brown <tyler@tmatthew.net>
2016-02-05 18:09:03 -0500

    use redux logger on client

--- app/configure-store.js
+++ app/configure-store.js
@@ -3,18 +3,20 @@ import promiseMiddleware from 'redux-promise-middleware';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 
-let logger = createLogger({logger: console, collapsed: () => true});
-
+let middleware = [promiseMiddleware()];
 let initialState = __IS_CLIENT__
   ? window.__data
   : {};
 
+// TODO: only do this in dev
+if (__IS_CLIENT__) {
+  let logger = createLogger({logger: console, collapsed: () => true});
+  middleware.push(logger);
+}
+
 export default function() {
   return createStore(rootReducer, initialState, compose(
-    applyMiddleware(
-      promiseMiddleware()
-      // logger
-    ),
+    applyMiddleware(...middleware),
     typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
   ));  
 };
