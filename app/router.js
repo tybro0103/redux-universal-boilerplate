5f3bd23808ed8d8c54aa472380f316f94e14b593
Tyler Brown <tyler@tmatthew.net>
2016-02-23 23:26:06 -0500

    send location to routes

--- app/router.js
+++ app/router.js
@@ -6,26 +6,18 @@ export default function buildRouter(store) {
   router.setContext({store});
 
   router.use('/', (done, location, context) => {
-    console.log('~~~~~~~~~ context', context);
-    console.log('~~~~~~~~~ location', location);
     done.ok({page: 'home'});
   });
 
   router.use('/people', (done, location, context) => {
-    console.log('~~~~~~~~~ context', context);
-    console.log('~~~~~~~~~ location', location);
     done.redirect('/home');
   });
 
   router.use('/planets/:planetId', (done, location, context) => {
-    console.log('~~~~~~~~~ context', context);
-    console.log('~~~~~~~~~ location', location);
     done.error({message: 'whoops'});
   });
 
   router.use('/planets', (done, location, context) => {
-    console.log('~~~~~~~~~ context', context);
-    console.log('~~~~~~~~~ location', location);
     done.ok({page: 'planets-index'});
   });
 
